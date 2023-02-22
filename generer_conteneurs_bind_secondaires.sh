10cc681446af50ce811fe156bd5665b58d305379
Croz <80237957+Crozbar@users.noreply.github.com>
2023-02-21 22:32:52 -0500

    Update generer_conteneurs_bind_secondaires.sh

--- generer_conteneurs_bind_secondaires.sh
+++ generer_conteneurs_bind_secondaires.sh
@@ -4,7 +4,7 @@
 for i in $(seq $1)
 do
   docker container rm "bind9_sec_DMZ_$i"  --force 2>/dev/null 1>&2 && echo -e "\nLe conteneur bind9_sec_DMZ_$i est prêt à être remplacé" || echo " "
-  docker run -d -it --name "bind9_sec_DMZ_$i" --network macnet64 -p "53:53/tcp" -p "53:53/udp" -v /dns-secondaire1/zones:/var/lib/bind -v /dns-secondaire1/named.d:/etc/bind ubuntu/bind9 /bin/bash && echo "Création du conteneur bind9_sec_DMZ_$i"
+  docker run -d -it --name "bind9_sec_DMZ_$i" --network macnet64 -v /dns-secondaire1/zones:/var/lib/bind -v /dns-secondaire1/named.d:/etc/bind ubuntu/bind9 /bin/bash && echo "Création du conteneur bind9_sec_DMZ_$i"
   docker exec -it "bind9_sec_DMZ_$i" chown bind:bind /var/lib/bind
   docker exec -it "bind9_sec_DMZ_$i" service named start
 done
